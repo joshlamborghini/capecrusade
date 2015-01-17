2a74ed28218381baf57fa4d5d67dcff3bef634ed
Mike Chabot <mike.chabot@live.com>
2015-01-17 11:27:28 -0500

    angularizing

--- src/main/webapp/js/angular-crusade.js
+++ src/main/webapp/js/angular-crusade.js
@@ -1,10 +1,57 @@
 var app = angular.module('capeCrusade', ['ui.bootstrap']);
 
-app.controller('CrusadeCtrl', ['$scope', function($scope) {
+app.controller('CrusadeCtrl', ['$scope', '$sce', function($scope, $sce) {
 
-  $scope.tabs = [
-    { title:'Dynamic Title 1', content:'Dynamic content 1' },
-    { title:'Dynamic Title 2', content:'Dynamic content 2', disabled: true }
-  ];
+  $scope.data = {
+    name: 'Cape Crusade',
+    backgroundUrl: '/img/beach.jpg',
+    images: {
+      logo: {
+        normal: '/img/cclogo.png'
+      },
+      shield: {
+        normal: '/img/shield.png',
+        small: '/img/shield-sm.png'
+      }
+    },
+    dates: [
+      "May 30th & 31st",
+      "June 6th & 7th"
+    ],
+    videos: [
+      {
+        url: $sce.trustAsResourceUrl('//player.vimeo.com/video/53171530?portrait=0&byline=0&title=0')
+      },
+      {
+        url: $sce.trustAsResourceUrl('//player.vimeo.com/video/53171530?portrait=0&byline=0&title=0')
+      }
+    ],
+    links: [
+      {
+        name: 'About',
+        url: ''
+      },
+      {
+        name: 'Sign Up',
+        url: ''
+      },
+      {
+        name: 'Sponsors',
+        url: ''
+      },
+      {
+        name: 'Schedule',
+        url: ''
+      },
+      {
+        name: 'Donate',
+        url: ''
+      }
+    ]
+  };
 
+}]);
+
+app.controller('CarouselCtrl', ['$scope', function ($scope) {
+  $scope.carousel = {};
 }]);
